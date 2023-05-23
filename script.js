ffc0debc2a334e0048daa20b7488d6c17e518867
Im-Humor <orangite@gmail.com>
2023-05-22 20:54:19 -0700

    add hover function which changes background color and grid size prompt

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let sqrContainer = document.querySelector(".sqr-container");
 
-for(x = 0; x < 16; x++) {
+let gridPrompt = parseInt(prompt("What size grid?"), 10);
+
+for(x = 0; x < gridPrompt; x++) {
     let sqrRow = document.createElement("div");
     sqrRow.classList.add("sqr-row");
     sqrRow.style.display = 'flex';
     sqrRow.style.flex = '1';
     sqrContainer.appendChild(sqrRow);
-    for(y = 0; y < 16; y++) {
+    for(y = 0; y < gridPrompt; y++) {
         let square = document.createElement("div");
         square.classList.add("square");
         let sqrStyle = square.style
@@ -17,5 +19,9 @@ for(x = 0; x < 16; x++) {
     }
 }
 
-//create eventlisteners for hover to toggle background color change
-//will i need to also create an event listener for stopping hovering?
+const squares = document.querySelectorAll('.square');
+squares.forEach(element => {
+    element.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = 'darkgrey';
+    })
+})
